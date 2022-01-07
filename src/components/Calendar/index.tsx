3e39476bce4fb6b223aaa2a25aee53713530885c
thallysfs <thallys@hotmail.com>
2022-01-07 13:21:41 -0300

    componente calendário parte 2

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,12 +1,51 @@
 import React from 'react'
+import { Feather } from '@expo/vector-icons'
+import { useTheme } from 'styled-components'
 
-//renomeando o componente porque 
-import { Calendar as CustomCalendar } from 'react-native-calendars'
+//renomeando o componente porque tem o mesmo nome do meu componente
+import { Calendar as CustomCalendar, LocaleConfig } from 'react-native-calendars'
+
+//minha configuração local para a tradução pt-br do calendário
+import { ptBR } from './localeConfig'
+
+LocaleConfig.locales['pt-BR'] = ptBR;
+LocaleConfig.defaultLocale = 'pt-BR'
 
 export function Calendar(){
+  const theme = useTheme();  
+
   return(
-    <CustomCalendar>
-        
-    </CustomCalendar>
+    <CustomCalendar 
+        renderArrow={( direction ) => 
+            <Feather 
+                size={24}
+                color={theme.colors.shape}
+                name={ direction == 'left' ? "chevron-left" : "chevron-right"}
+            />
+        }
+
+        headerStyle={{
+            backgroundColor: theme.colors.background_secondary,
+            borderBottomWidth: 0.5,
+            borderBottomColor: theme.colors.text_detail,
+            paddingBottom: 10,
+            marginBottom: 10
+        }}
+
+        theme={{
+            textDayFontFamily: theme.fonts.primary_400,
+            textDayHeaderFontFamily: theme.fonts.primary_500,
+            textDayHeaderFontSize: 10,
+            textMonthFontFamily: theme.fonts.secondary_600,
+            textMonthFontSize: 20,
+            monthTextColor: theme.colors.title,
+            arrowStyle: {
+                marginHorizontal: -15
+            }
+        }}
+
+        firstDay={1}
+        minDate={new Date().toString()}
+    />
   )
 }
