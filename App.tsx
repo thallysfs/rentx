5352382ee3e3166a2e6072694275f6cc2737c6f3
thallysfs <thallys@hotmail.com>
2022-01-07 14:30:45 -0300

    Screen schedulingCompleted e component ConfirmButton

--- App.tsx
+++ App.tsx
@@ -14,7 +14,7 @@ import {
 } from '@expo-google-fonts/archivo'
 
 
-import { SchedulingDetails } from './src/screens/SchedulingDetails';
+import { SchedulingComplete } from './src/screens/SchedulingComplete'
 import theme from './src/styles/theme';
 
 export default function App() {
@@ -32,7 +32,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>   
-      <SchedulingDetails />
+      <SchedulingComplete />
     </ThemeProvider>
   );
 }
---
+++ src/screens/SchedulingComplete/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useWindowDimensions } from 'react-native'
+import LogoSvg from '../../assets/logo_background_gray.svg';
+import DoneSvg from '../../assets/done.svg';
+
+import { Container, Content, Footer, Message, Title } from './styles'
+import { ConfirmButton } from '../../components/ConfirmButton';
+
+export function SchedulingComplete(){
+    const { width } = useWindowDimensions();
+
+  return(
+    <Container>
+      <LogoSvg width={width} />
+
+      <Content>
+          <DoneSvg width={80} height={80} />
+          <Title>Carro Alugado!</Title>
+
+          <Message>
+              Agora você só precisa ir {'\n'}
+              até a concessionária da RENTX {'\n'}
+              pegar seu automóvel.
+          </Message>
+
+          <Footer>
+              <ConfirmButton title="OK"/>
+          </Footer>
+
+          
+      </Content>
+    </Container>
+  )
+}
