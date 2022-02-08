c666ffb3ffab2c2cdf18e64165c58f08a7d3b70e
thallysfs <thallys@hotmail.com>
2022-02-08 14:40:02 -0300

    acessibilidade do formulário

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
-import { StatusBar } from 'react-native'
+import { 
+  StatusBar,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard 
+} from 'react-native'
 import { useTheme } from 'styled-components';
 
 import { Button } from '../../components/Button';
@@ -19,52 +24,58 @@ export function Signin(){
   const theme = useTheme();
 
   return(
-    <Container>
-      <StatusBar 
-        barStyle='dark-content'
-        backgroundColor='transparent'
-        translucent
-      />
-      <Header>
-        <Title>Estamos{'\n'}
-          quase lá.
-        </Title>
-        <SubTitle>
-          Faça seu login para começar {'\n'}
-          uma experiência incrível. 
-        </SubTitle>
-      </Header>
-      <Form>
-        <Input
-          iconName='mail'
-          placeholder='E-mail'
-          keyboardType='email-address'
-          autoCorrect={false}
-          autoCapitalize="none"
-        />
-        <PasswordInput
-          iconName='lock'
-          placeholder='Senha'
-        />
-      </Form>
+    <KeyboardAvoidingView behavior='position' enabled>
+      {/* O TouchableWithoutFeedback captura toques na tela, quando capturar, o onPress ativa o
+      fechamento do teclado */}
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <StatusBar 
+            barStyle='dark-content'
+            backgroundColor='transparent'
+            translucent
+          />
+          <Header>
+            <Title>Estamos{'\n'}
+              quase lá.
+            </Title>
+            <SubTitle>
+              Faça seu login para começar {'\n'}
+              uma experiência incrível. 
+            </SubTitle>
+          </Header>
+          <Form>
+            <Input
+              iconName='mail'
+              placeholder='E-mail'
+              keyboardType='email-address'
+              autoCorrect={false}
+              autoCapitalize="none"
+            />
+            <PasswordInput
+              iconName='lock'
+              placeholder='Senha'
+            />
+          </Form>
 
-      <Footer>
-        <Button
-          title='Login'
-          onPress={() => {}}
-          enabled={false}
-          loading={false}
-        />        
-        <Button
-          title='Criar conta gratuita'
-          color={theme.colors.background_secondary}
-          light
-          onPress={() => {}}
-          enabled={false}
-          loading={false}
-        />
-      </Footer>
+          <Footer>
+            <Button
+              title='Login'
+              onPress={() => {}}
+              enabled={false}
+              loading={false}
+            />        
+            <Button
+              title='Criar conta gratuita'
+              color={theme.colors.background_secondary}
+              light
+              onPress={() => {}}
+              enabled={false}
+              loading={false}
+            />
+          </Footer>
 
-    </Container>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   )
 }
